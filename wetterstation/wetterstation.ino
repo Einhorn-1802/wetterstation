13659c04acfc35532f0038c615520ef700bc0349
Einhorn-1802 <60885058+Einhorn-1802@users.noreply.github.com>
2022-02-27 17:47:37 +0100

    before changing the time

--- wetterstation/wetterstation.ino
+++ wetterstation/wetterstation.ino
@@ -3,52 +3,102 @@
 #include <WiFiUdp.h>
 #include "wlanData.h"
 #include "myTime.h"
+#include "utcMemory.h"
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <BlynkSimpleEsp8266.h>
+
+#define RTCMEMORYSTART 65
+
+
+extern "C" {
+#include "user_interface.h"
+}
+
+#define BLYNK_PRINT Serial
+
+#define ONE_WIRE_PIN 12
+ 
+OneWire oneWire(ONE_WIRE_PIN);
+DallasTemperature sensors(&oneWire);
+
+char auth[] = BLYNK_AUTH_TOKEN;
+#define BLYNK_FIRMWARE_VERSION "0.0.1"
+
+typedef struct {
+  int rainCount;
+  int year;
+  int y_day;
+} rtcStore;
+
+rtcStore rtcMem;
+unsigned long epochTime;
+
+int wakeupReason;
+
+void readFromRTCMemory() {
+  system_rtc_mem_read(RTCMEMORYSTART, &rtcMem, sizeof(rtcMem));
+  yield();
+}
+void writeToRTCMemory() {
+  system_rtc_mem_write(RTCMEMORYSTART, &rtcMem, sizeof(rtcMem));
+  yield();
+}
 
 void setup() {
   // put your setup code here, to run once:
     Serial.begin(115200);         // Start the Serial communication to send messages to the computer
-    connectWifi();
+    wakeupReason = digitalRead(13);
+    readFromRTCMemory();
+    Serial.println("");
+    Serial.print("Wakeup Reason:");
+    Serial.println(wakeupReason);
+    Serial.println(rtcMem.rainCount);
+    Serial.println(rtcMem.year);
+    Serial.println(rtcMem.y_day);
+    if (wakeupReason == 0) {
+      rtcMem.rainCount++;
+    }    
+    Blynk.begin(auth, ssid, password);
+    pinMode(D4, OUTPUT);
     timeClient.begin();
-    
-    
-    
+    timeClient.update();
+    epochTime = timeClient.getEpochTime();
+    struct tm *ptm = gmtime ((time_t *)&epochTime);
+    int current_year = ptm->tm_year + 1900;
+    int year_day = ptm-> tm_yday;
+    int day_hour = ptm-> tm_hour;
+    Serial.print("Epoch Time: ");
+    Serial.println(epochTime);
+    Serial.print("Current Year: ");
+    Serial.println(current_year);
+    Serial.print("Day of the Year: ");
+    Serial.println(year_day);
+
+    if (current_year == rtcMem.year && year_day == rtcMem.y_day) {
+    }
+    else {
+      if (wakeupReason == 0) {
+        rtcMem.rainCount = 1;
+      }
+      else {
+        rtcMem.rainCount = 0;
+      }
+      rtcMem.year = current_year;
+      rtcMem.y_day = year_day;
+      writeToRTCMemory();
+    } 
+    sensors.begin();
+    sensors.requestTemperatures();
+    Serial.println(sensors.getTempCByIndex(0));
+    Blynk.virtualWrite(V0, sensors.getTempCByIndex(0));
+    Blynk.virtualWrite(V1, (double)rtcMem.rainCount * 0.25);
+    delay(100);
+//    ESP.deepSleep(1e6*30);
+    ESP.deepSleep(0);
 
 }
 
-void connectWifi() {
-   Serial.println('\n');
-  
-   WiFi.begin(ssid, password);             // Connect to the network
-   Serial.print("Connecting to ");
-   Serial.print(ssid); Serial.println(" ...");
-
-   int i = 0;
-   while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
-   delay(1000);
-   Serial.print(++i); Serial.print(' ');
-   }
-
-   Serial.println('\n');
-   Serial.println("Connection established!");  
-   Serial.print("IP address:\t");
-   Serial.println(WiFi.localIP());         // Send the IP address of the ESP8266 to the computer
-
-};
-
-
 void loop() {
-  // put your main code here, to run repeatedly:
-    timeClient.update();
-
-  Serial.print(daysOfTheWeek[timeClient.getDay()]);
-  Serial.print(", ");
-  Serial.print(timeClient.getHours());
-  Serial.print(":");
-  Serial.print(timeClient.getMinutes());
-  Serial.print(":");
-  Serial.println(timeClient.getSeconds());
-  Serial.println(timeClient.getFormattedTime());
-
-  delay(1000);
 
 }
